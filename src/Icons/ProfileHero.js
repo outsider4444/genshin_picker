52004b4abefb0cbb0bd5729e02ab80c0c74c2fd7
Danila Kuzin <danila_kuzin_2001@mail.ru>
2024-07-27 14:08:10 +0400

    Обновлена верстка, добавлена строчка для введения уровня персонажа, написан метод увеличения консты персонажа

--- src/Icons/ProfileHero.js
+++ src/Icons/ProfileHero.js
@@ -2,11 +2,26 @@ import React from 'react';
 import styles from "./Icon.module.css";
 
 const ProfileHero = ({
+                         userCharactersList, setUserCharactersList, id,
                          image = "https://genshin-info.ru/upload/resize_cache/iblock/9a8/w4vrp5t2uh6px1u2ovpoloa6tsglw7gh/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/Klorinda.webp",
-                         level = 0, consta = 0,
+                         level = 0,
                          size = 70, banned = false, marginTop = "15px",
                          backgroundColor = '#ed7819', float = "left", action, disabled, heroName = "Имя"
                      }) => {
+    const character = userCharactersList.find(character => character.heroId === id);
+    const consta = character ? character.consta : 0;
+
+    function increaseConst(index) {
+        const nextConst = userCharactersList.map(character => {
+            if (character.heroId === index) {
+                return { ...character, consta: character.consta + 1 };
+            } else {
+                return character;
+            }
+        });
+        setUserCharactersList(nextConst);
+    }
+
     return (
         <div className={styles.roundIcon}
              style={{
@@ -14,12 +29,41 @@ const ProfileHero = ({
                  borderRadius: "10%", display: "flex", float: float, marginTop
              }}>
             <div>
-                <div style={{position: "absolute"}}>
-                    <button className={styles.constIncreaseBtn}>
-                        +
-                    </button>
+                <div style={{
+                    borderRadius: "10%",
+                    position: "absolute",
+                    textAlign: "center",
+                    display: "grid",
+                    alignItems: "end",
+                    backgroundColor: backgroundColor !== "grey" ? "rgb(45 50 90)" : "white",
+                    height: "35px",
+                    width: size - 20,
+                    marginTop: "-25px",
+                    zIndex: "-2",
+                }}>
+                    <div style={{display: "grid", gridTemplateColumns: "50% 50%", columnGap: "24px"}}>
+                        <input disabled value={level} type="text"
+                               style={{width: "20px", height: "10px", marginBottom: "15px", marginLeft: "10px"}}
+                               hidden={backgroundColor === "grey"}/>
+                        <button style={{
+                            width: "15px",
+                            height: "15px",
+                            borderRadius: "40%",
+                            border: "none",
+                            marginBottom: "15px",
+                            fontWeight: "bold"
+                        }}
+                                hidden={backgroundColor === "grey"}>Р
+                        </button>
+                    </div>
                 </div>
-                <div style={{position: "absolute", float: "right", textAlign: "right", width: "50px"}}>
+                <button className={styles.constIncreaseBtn} style={{position: "absolute", zIndex: "0"}}
+                        onClick={() => {
+                            increaseConst(id)
+                        }} hidden={backgroundColor === "grey"}>
+                    +
+                </button>
+                <div style={{display: "grid", gridTemplateColumns: "50% 40%", width: size, position: "absolute", height: "2px"}}>
                     <div style={{
                         fontSize: size > 70 ? "auto" : "10pt",
                         fontWeight: "bold",
@@ -29,8 +73,6 @@ const ProfileHero = ({
                     }}>
                         С{consta}
                     </div>
-                </div>
-                <div style={{position: "absolute", float: "right", textAlign: "right", width: "80px"}}>
                     <div style={{
                         fontSize: size > 70 ? "auto" : "10pt",
                         fontWeight: "bold",
@@ -46,23 +88,15 @@ const ProfileHero = ({
                  style={{borderRadius: "10%", position: "relative", marginTop: "20px", zIndex: "1"}} src={image}
                  alt=""/>
             <div style={{
-                borderRadius: "10%",
-                position: "absolute",
-                textAlign: "center",
-                display: "grid",
-                gridTemplateColumns: "20% 80%",
-                alignItems: "end",
-                backgroundColor: "rgb(45 50 90)",
-                height: "35px",
-                width: "100px",
-                marginTop: size > 70 ? "90px" : "60px",
+                borderRadius: "10%", position: "absolute",
+                textAlign: "center", display: "grid",
+                alignItems: "end", backgroundColor: "rgb(45 50 90)",
+                height: "35px", width: size,
+                marginTop: size - 15,
                 zIndex: "-1"
             }}>
-                <button className={styles.constIncreaseBtn}>
-                    +
-                </button>
                 <div
-                    style={{fontSize: size > 70 ? "auto" : "10pt", fontWeight: "bold", color: "white"}}>
+                    style={{fontSize: (size / 10) + 2, fontWeight: "bold", color: "white"}}>
                     {heroName}
                 </div>
             </div>
--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -21,46 +21,56 @@ const Profile = ({token, userInfo, setUserInfo, userCharactersList, setUserChara
             .catch(error => {
                 console.error("Error fetching heroes:", error);
             });
-    }, [])
+    }, []);
 
-    const onClickHero = (id) => {
+    const onClickHero = async (id) => {
         const character = heroesList.find(c => c.id === id);
         const userCharacter = userCharactersList.find(c => c.heroId === id);
         const inArray = userCharactersList.some(c => c.heroId === id);
 
         if (inArray) {
-            axios.delete("http://localhost:8080/api/v1/users_hero/" + userCharacter.id, {
+            axios.delete(`http://localhost:8080/api/v1/users_hero/${userCharacter.id}`, {
                 headers: {
                     'Authorization': `Bearer ${token}`
                 }
-            }).then(setUserCharactersList(userCharactersList.filter(c => c.heroId !== character.id)));
-        }
-        else {
+            }).then(() => {
+                setUserCharactersList(prevState => prevState.filter(c => c.heroId !== character.id));
+            }).catch(error => {
+                console.error("Error deleting hero:", error);
+            });
+        } else {
             let newHero = {
                 "userId": userInfo.id,
                 "heroId": character.id,
                 "level": 0,
                 "consta": 0
-            }
+            };
 
             axios.post("http://localhost:8080/api/v1/users_hero", newHero, {
                 headers: {
                     'Authorization': `Bearer ${token}`
                 }
-            }).then(setUserCharactersList([...userCharactersList, newHero]))
+            }).then(response => {
+                const createdHero = response.data;
+                setUserCharactersList(prevState => [...prevState, createdHero]);
+            }).catch(error => {
+                console.error("Error adding hero:", error);
+            });
         }
-    }
+    };
 
     const listCharacter = heroesList.map(character => {
         const isInUserList = userCharactersList.some(c => c.heroId === character.id);
 
         return (
             <ProfileHero
+                userCharactersList={userCharactersList}
+                setUserCharactersList={setUserCharactersList}
+                id={character.id}
                 level={character.level}
-                consta={character.consta}
                 backgroundColor={!isInUserList ? "grey" : (character.starCount === 4 ? "#836dad" : "#ed7819")}
                 disabled={false}
-                size={100}
+                size={140}
                 heroName={character.name}
                 key={character.id}
                 image={character.photoPath}
@@ -75,31 +85,29 @@ const Profile = ({token, userInfo, setUserInfo, userCharactersList, setUserChara
 
     return (
         <div>
-            <div>
-                <div className={styles.profile} style={{height: "250px"}}>
-                    <div></div>
-                    <div>
-                        <img
-                            src="https://genshin-info.ru/upload/resize_cache/iblock/9a8/w4vrp5t2uh6px1u2ovpoloa6tsglw7gh/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/Klorinda.webp"
-                            alt=""
-                            style={{
-                                width: "200px",
-                                borderRadius: "50%",
-                                border: "5px solid black"
-                            }}
-                        />
-                        <div>{userInfo.username}</div>
-                    </div>
-                    <div>
-                        <button className={styles.changeBtn}>Изменить профиль</button>
-                    </div>
+            <div className={styles.profile} style={{height: "250px"}}>
+                <div></div>
+                <div>
+                    <img
+                        src="https://genshin-info.ru/upload/resize_cache/iblock/9a8/w4vrp5t2uh6px1u2ovpoloa6tsglw7gh/200_200_1d7a58ff99b324185ccb5ad5dfbdb5e85/Klorinda.webp"
+                        alt=""
+                        style={{
+                            width: "200px",
+                            borderRadius: "50%",
+                            border: "5px solid black"
+                        }}
+                    />
+                    <div>{userInfo.username}</div>
                 </div>
-                <div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
-                    <div style={{height: "1px", width: "60%", background: "black"}}></div>
+                <div>
+                    <button className={styles.changeBtn}>Изменить профиль</button>
                 </div>
-                <div style={{marginLeft: "25%"}}>
-                    {listCharacter}
-                </div>
+            </div>
+            <div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+                <div style={{height: "1px", width: "60%", background: "black"}}></div>
+            </div>
+            <div className={styles.characters}>
+                {listCharacter}
             </div>
         </div>
     );
